4bdc5f155458b11b3f6e982271a2fe36592d075d
Matthias Mees <mm@yellowled.de>
2015-11-30 20:45:57 +0100

    Add Jekyll template
    Keep your fingers crossed, everyone!

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    // Smallscreen nav
+    var nav = responsiveNav('.nav-collapse', {
+        customToggle: "#open-nav",
+        closeOnNavClick: true,
+    });
+
+    // Skip links
+    $('#to-top, #table-of-contents a').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 300);
+                return false;
+            }
+        }
+    });
+
+    // Lightboxed image gallery
+    $('.image-gallery').magnificPopup({
+        delegate: 'a',
+        gallery: {
+            enabled: true,
+            tCounter: '<span class="mfp-counter">%curr%/%total%</span>' // markup of counter
+        },
+        type: 'image'
+    });
+});
