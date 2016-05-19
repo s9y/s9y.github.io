87c6c7fca154f102da7b554be41726daa0e4980b
Garvin Hicking <blog@garv.in>
2016-05-19 13:37:18 +0200

    a bit more :)

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
               this.feedparent.find('ol').empty();
               for(var i=0; i < 4; i++) {
                   if (feed.items[i]) {
-                     var shortenedDescription = $.trim(feed.items[i].description).substring(0, 110).split(' ').slice(0, -1).join(' ') + "...";
+                     var shortenedDescription = $.trim(feed.items[i].description).substring(0, 250).split(' ').slice(0, -1).join(' ') + "...";
                      this.feedparent.find('ol').append('<li><h3><a href="' + feed.items[i].link + '">' + feed.items[i].title + '</a></h3><p>' + shortenedDescription + '</p></li>');
                   }
               }
