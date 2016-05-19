40de54952113985729bae90259a5d4080bf52655
Garvin Hicking <blog@garv.in>
2016-05-19 12:54:48 +0200

    fill in

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,6 +7,13 @@ $(document).ready(function() {
             success: function(feed) {
               console.log(feed);
               console.log(this.feedparent);
+              
+              this.feedparent.find('ol').empty();
+              for(var i=0; i <= 4; i++) {
+                  if (feed.items[i]) {
+                     this.feedparent.find('ol').append('<li><h3><a href="' + feed.items[i].link + '">' + feed.items[i].title + '</a></h3><p>' + feed.items[i].description + '</p></li>');
+                  }
+              }
             }
         });
     });    
