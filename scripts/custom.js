dfd0594c40d0acf7dd294f30d6917032dfb1ea1f
Garvin Hicking <blog@garv.in>
2016-05-19 13:36:28 +0200

    remove formatting, truncate chars

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $('*[data-rss*="http"]').each(function() {
-        jQuery.getFeed({
+        $.getFeed({
             url: $(this).attr('data-rss'),
             feedparent: $(this),
             success: function(feed) {
               this.feedparent.find('ol').empty();
               for(var i=0; i < 4; i++) {
                   if (feed.items[i]) {
-                     this.feedparent.find('ol').append('<li><h3><a href="' + feed.items[i].link + '">' + feed.items[i].title + '</a></h3><p>' + feed.items[i].description + '</p></li>');
+                     var shortenedDescription = $.trim(feed.items[i].description).substring(0, 110).split(' ').slice(0, -1).join(' ') + "...";
+                     this.feedparent.find('ol').append('<li><h3><a href="' + feed.items[i].link + '">' + feed.items[i].title + '</a></h3><p>' + shortenedDescription + '</p></li>');
                   }
               }
             }
